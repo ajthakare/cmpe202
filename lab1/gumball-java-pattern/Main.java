11fe1911a76bcb3b03d40a54f659d9757c67b676
Ajinkya Thakare <thakare725@gmail.com>
2018-09-08 18:51:38 -0700

    Minor changes
    Added comment in main  class

--- lab1/gumball-java-pattern/Main.java
+++ lab1/gumball-java-pattern/Main.java
@@ -3,6 +3,10 @@
 public class Main {
 
 	public static void main(String[] args) {
+		
+		//initialization of three types of gumball machines. Only one machine can be used at a time. Uncomment out the machine object you want
+		//to use here and comment in the other machine objects you don't want to use.
+		
 		GumballMachine gumballMachine = new GumballMachine(5,"TypeA");
 		//GumballMachine gumballMachine = new GumballMachine(5,"TypeB");
 		//GumballMachine gumballMachine = new GumballMachine(2,"TypeC");
