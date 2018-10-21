8b2b3c5361016f6d7f2f730531ba8471beece9b3
Ajinkya Thakare <42667147+ajthakare@users.noreply.github.com>
2018-10-20 22:45:31 -0700

    added uml class diagram and output file

--- lab7/src/Client.java
+++ lab7/src/Client.java
@@ -2,7 +2,7 @@
 
 public class Client {
 
-    public static void run()
+    public static void runTest()
     {
         Component theOrder = BuildOrder.getOrder() ;
         theOrder.printDescription();
